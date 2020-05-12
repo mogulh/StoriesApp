e467cb5de5e9a4e7d43d1ae9ae165536e3ba9302
mogulh <mogulhkiti@gmail.com>
2020-05-12 07:56:08 +0300

    complete home layout + profile +altprofile

---
+++ app/src/main/java/com/mogulh/stories/AltProfile.java
@@ -0,0 +1,14 @@
+package com.mogulh.stories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AltProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alt_profile);
+    }
+}
